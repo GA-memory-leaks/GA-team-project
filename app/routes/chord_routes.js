de280a8d239ab06bdffe6bf12ec91adfcc9f11ce
CaldoNic7 <caldwellnicholaslv@gmail.com>
2021-08-24 10:15:19 -0400

    Create model and routes
    Created 'Chord' model and CRUD routes. Updated server.js. @TommyCravens @NathanFeldman

---
+++ app/routes/chord_routes.js
@@ -0,0 +1,115 @@
+// Express docs: http://expressjs.com/en/api.html
+const express = require('express')
+// Passport docs: http://www.passportjs.org/docs/
+const passport = require('passport')
+
+// pull in Mongoose model for chords
+const Chord = require('../models/chord')
+
+// this is a collection of methods that help us detect situations when we need
+// to throw a custom error
+const customErrors = require('../../lib/custom_errors')
+
+// we'll use this function to send 404 when non-existant document is requested
+const handle404 = customErrors.handle404
+// we'll use this function to send 401 when a user tries to modify a resource
+// that's owned by someone else
+const requireOwnership = customErrors.requireOwnership
+
+// this is middleware that will remove blank fields from `req.body`, e.g.
+// { chord: { title: '', body: 'foo' } } -> { chord: { body: 'foo' } }
+const removeBlanks = require('../../lib/remove_blank_fields')
+// passing this as a second argument to `router.<verb>` will make it
+// so that a token MUST be passed for that route to be available
+// it will also set `req.user`
+const requireToken = passport.authenticate('bearer', { session: false })
+
+// instantiate a router (mini app that only handles routes)
+const router = express.Router()
+
+// INDEX
+// GET /chords
+router.get('/chords', requireToken, (req, res, next) => {
+  Chord.find()
+    .then((chords) => {
+      // `chords` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return chords.map((chord) => chord.toObject())
+    })
+  // respond with status 200 and JSON of the chords
+    .then((chords) => res.status(200).json({ chords: chords }))
+  // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// SHOW
+// GET /chords/5a7db6c74d55bc51bdf39793
+router.get('/chords/:id', requireToken, (req, res, next) => {
+  // req.params.id will be set based on the `:id` in the route
+  Chord.findById(req.params.id)
+    .then(handle404)
+  // if `findById` is succesful, respond with 200 and "chord" JSON
+    .then((chord) => res.status(200).json({ chord: chord.toObject() }))
+  // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// CREATE
+// POST /chords
+router.post('/chords', requireToken, (req, res, next) => {
+  // set owner of new chord to be current user
+  req.body.chord.owner = req.user.id
+
+  Chord.create(req.body.chord)
+  // respond to succesful `create` with status 201 and JSON of new "chord"
+    .then((chord) => {
+      res.status(201).json({ chord: chord.toObject() })
+    })
+  // if an error occurs, pass it off to our error handler
+  // the error handler needs the error message and the `res` object so that it
+  // can send an error message back to the client
+    .catch(next)
+})
+
+// UPDATE
+// PATCH /chords/5a7db6c74d55bc51bdf39793
+router.patch('/chords/:id', requireToken, removeBlanks, (req, res, next) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.chord.owner
+
+  Chord.findById(req.params.id)
+    .then(handle404)
+    .then((chord) => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, chord)
+
+      // pass the result of Mongoose's `.update` to the next `.then`
+      return chord.updateOne(req.body.chord)
+    })
+  // if that succeeded, return 204 and no JSON
+    .then(() => res.sendStatus(204))
+  // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// DESTROY
+// DELETE /chords/5a7db6c74d55bc51bdf39793
+router.delete('/chords/:id', requireToken, (req, res, next) => {
+  Chord.findById(req.params.id)
+    .then(handle404)
+    .then((chord) => {
+      // throw an error if current user doesn't own `chord`
+      requireOwnership(req, chord)
+      // delete the chord ONLY IF the above didn't throw
+      chord.deleteOne()
+    })
+  // send back 204 and no content if the deletion succeeded
+    .then(() => res.sendStatus(204))
+  // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+module.exports = router
